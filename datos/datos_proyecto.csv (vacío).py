02d5df9c8e650146a85d445de6087b4220003e28
Sofía Bernini <sofiabelenbernini@gmail.com>
2026-04-05 17:08:17 -0300

    datos del proyecto

---
+++ datos/datos_proyecto.csv (vacío).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 19:57:27 2026
+
+@author: sofia
+"""
+
+#Modelo de datos esperado:
+    #Por el momento queda vacío porque todavía no tenemos los datos
+
+registro_participante = {
+"id_participante": #int,
+"tiempo": []
+"valor": []
+"fase": []
+"condicion_experimental": []
+"hit": []
+}
+datos = [#registro_participante1, registro_participante2, ...]
