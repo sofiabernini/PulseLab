a6010055ad141561043d7b275972482f91e4fdc4
sofiabernini <sofiabelenbernini@gmail.com>
2026-04-05 21:02:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
 """
 Created on Sun Apr  5 20:30:13 2026
 
@@ -28,34 +26,3 @@ if datos_participante:
    print(f"Frecuencia Cardíaca: {frecuencia_cardiaca:} BPM")
 else:
     print(f"No se encontraron datos para el participante {id_objetivo}.")
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  5 20:30:13 2026
-
-@author: sofia
-"""
-
-from src.carga_datos import cargar_datos
-from src.procesamiento_datos import filtrar_datos
-from src.metricas import calcular_promedio_senal, calcular_fc_desde_datos
-
-ruta = "datos/datos_proyecto.csv"
-    
-todos_los_datos = cargar_datos(ruta)
-    
-# Seleccionamos un participante específico para el análisis 
-id_objetivo = 1
-datos_participante = filtrar_datos(todos_los_datos, id_objetivo)
-   
-if datos_participante:
-   promedio_ecg = calcular_promedio_senal(datos_participante["valor"])
-        
-   frecuencia_cardiaca = calcular_fc_desde_datos(datos_participante)
-        
-   print(f"Resultados del participante {id_objetivo}: ")
-   print(f"Promedio de la señal ECG: {promedio_ecg}")
-   print(f"Frecuencia Cardíaca: {frecuencia_cardiaca:} BPM")
-else:
-    print(f"No se encontraron datos para el participante {id_objetivo}.")
->>>>>>> 952f3c59c76b7355cea5891914e3e3e7885001ed
