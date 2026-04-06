33dfb8aa31d77063c6d1bca90eab69896a86f0fa
Sofía Bernini <sofiabelenbernini@gmail.com>
2026-04-05 21:17:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ from src.metricas import calcular_promedio_senal, calcular_fc_desde_datos
 ruta = "datos/datos_proyecto.csv"
     
 todos_los_datos = cargar_datos(ruta)
-    
-# Seleccionamos un participante específico para el análisis 
-id_objetivo = 1
-datos_participante = filtrar_datos(todos_los_datos, id_objetivo)
+
+for participante in todos_los_datos:
+    id = participante["id_participante"]
+    datos_participante = filtrar_datos(todos_los_datos, id)
    
-if datos_participante:
-   promedio_ecg = calcular_promedio_senal(datos_participante["valor"])
+    if datos_participante:
+       promedio_ecg = calcular_promedio_senal(datos_participante["valor"])
         
-   frecuencia_cardiaca = calcular_fc_desde_datos(datos_participante)
+       frecuencia_cardiaca = calcular_fc_desde_datos(datos_participante)
         
-   print(f"Resultados del participante {id_objetivo}: ")
-   print(f"Promedio de la señal ECG: {promedio_ecg}")
-   print(f"Frecuencia Cardíaca: {frecuencia_cardiaca:} BPM")
-else:
-    print(f"No se encontraron datos para el participante {id_objetivo}.")
+       print(f"Resultados del participante {id}: ")
+       print(f"Promedio de la señal ECG: {promedio_ecg}")
+       print(f"Frecuencia Cardíaca: {frecuencia_cardiaca} BPM")
+    else:
+        print(f"No se encontraron datos para el participante {id}.")
+
