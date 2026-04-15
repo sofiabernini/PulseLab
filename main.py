03081bfa42a90cc3d58b621351f26aa553c3062d
sofiabernini <sofiabelenbernini@gmail.com>
2026-04-15 12:51:10 -0300

    Update main.py
    Programa principal con manejo de errores

--- main.py
+++ main.py
@@ -4,29 +4,42 @@ Created on Sun Apr  5 20:30:13 2026
 @author: sofia
 """
 
-from src.carga_datos import cargar_datos
-from src.procesamiento_datos import filtrar_datos
-from src.metricas import calcular_promedio_senal, calcular_fc_desde_datos
+from src.cargar_datos import cargar_datos
+from src.filtrar_participante import filtrar_por_participante
+from src.metricas import calcular_promedio_senal, calcular_maximo_senal, calcular_minimo_senal, calcular_fc_desde_datos
 
 try:
     ruta = "datos/PulseLab_mock_data.csv"
-except Exception as e:
-    print (f"Error de tipo {e}")
+    todos_los_datos = cargar_datos(ruta)
     
-todos_los_datos = cargar_datos(ruta)
+except FileNotFoundError:
+    print(f"Error: El archivo en {ruta} no fue encontrado.")
+    
+except Exception as e:
+    print(f"Ocurrió un error inesperado durante la ejecución: {e}")
+    
+try:
+    id_a_analizar = int(input("Ingrese el ID del participante: "))
+    
+except TypeError:
+    raise TypeError ("El ID debe ser un número entero.")
 
-for participante in todos_los_datos:
-    id = participante["ID participante"]
-    datos_participante = filtrar_datos(todos_los_datos, id)
+
+datos_participante = filtrar_por_participante(todos_los_datos, id_a_analizar)
    
-    if datos_participante:
-       promedio_ecg = calcular_promedio_senal(datos_participante["valor"])
+promedio_ecg = calcular_promedio_senal(datos_participante)
         
-       frecuencia_cardiaca = calcular_fc_desde_datos(datos_participante)
+frecuencia_cardiaca = calcular_fc_desde_datos(datos_participante)
+
+maximo_senal = calcular_maximo_senal(datos_participante)
+
+minimo_senal = calcular_minimo_senal(datos_participante)
+
         
-       print(f"Resultados del participante {id}: ")
-       print(f"Promedio de la señal ECG: {promedio_ecg}")
-       print(f"Frecuencia Cardíaca: {frecuencia_cardiaca} BPM")
-    else:
-        print(f"No se encontraron datos para el participante {id}.")
+print(f"Resultados del participante {id_a_analizar}: ")
+print(f"Máximo de la señal ECG: {maximo_senal}")
+print(f"Mínimo de la señal ECG: {minimo_senal}")
+print(f"Promedio de la señal ECG: {promedio_ecg}")
+print(f"Frecuencia Cardíaca: {frecuencia_cardiaca} BPM")
+
 
