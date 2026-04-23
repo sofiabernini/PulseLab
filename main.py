ef8103316e421309bdc1f3bff30e97b32a8e1a6a
sofiabernini <sofiabelenbernini@gmail.com>
2026-04-23 15:52:45 -0300

    agrego return apara que se deje de ecutar el programa si hay un error

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def ejecutar_programa (id_trabajado,todos_los_datos):
             
     if datos_participante == None:
         print(f"El ID {id_trabajado} no se encuentra en la lista de participantes.")
-    
+        return
         
     promedio_ecg = calcular_promedio_senal(datos_participante["Valor ECG"])
                 
