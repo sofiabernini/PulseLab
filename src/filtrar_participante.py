02b36bcc870438bd31d45ff0aeeda4fb9948d90b
angiemarengo <marengoa@udesa.edu.ar>
2026-05-27 15:45:35 -0300

    Modificaciones en filtrar_participante
    Agregue manejo de errores y modificaciones en el docstring

--- src/filtrar_participante.py
+++ src/filtrar_participante.py
@@ -7,26 +7,29 @@ Created on Sun Apr  5 17:29:48 2026
 
 def filtrar_por_participante(df, id_participante):
     """
-    Busca, en el Data
+    Busca, en el DataFrame la(s) fila(s) del participante cuyo id es igual al id buscado.
 
     Parameters
     ----------
-    datos : lista
-        lista con los datos de los participantes en diccionarios distintos.
+    df: DataFrame
+        DataFrame con los datos de los participantes.
     id_participante : int
         Es el id del participante buscado.
 
     Returns
     -------
-    registro : dict
-        Es el diccionario con los datos del id del participante buscado.
+    filtrado : DataFrame
+        Es el DataFrame con los datos del participante cuyo id es el igual al id buscado.
     None
-        Si el id buscado no está en la lista datos.
+        Si el id buscado no está en el DataFrame.
     Raises: 
-        ValueError: Si la lista de datos está vacía
+        ValueError: Si el DataFrame está vacío.
 
     """
-    
+    if df.size==0:
+        raise ValueError ("Error. El DataFrame se encuentra vacío")
+    if id_participante not in df["id_participante"].values:
+        return None
     filtrado=df[df["id_participante"]==id_participante]
     return filtrado
     
