2a2e04c73f70431b456066dd97ca3d9778cebea9
sofiabernini <sofiabelenbernini@gmail.com>
2026-05-28 03:35:19 -0300

    actualización de la función que genera el gráfico

--- grafico_ECG_tiempo.py
+++ grafico_ECG_tiempo.py
@@ -6,21 +6,25 @@ Created on Wed May 27 21:57:43 2026
 """
 
 import matplotlib.pyplot as plt
+
 def grafico_ECG_tiempo(datos_participante):
     """
-    
+    Genera el gráfico y determina sus características visuales.
 
     Parameters
     ----------
     datos_participante : DataFrame
-       Es el DataFrame que contiene los datos del participante cuyo id es el id buscado .
+       Es el DataFrame que contiene los datos del participante cuyo id es el id buscado.
 
     Returns
     -------
     None.
 
     """
-    plt.figure()
-    plt.plot(datos_participante["Tiempo"], datos_participante["Valor ECG"])
-    plt.title("Gráfico de valores de ECG en función del tiempo")
-    plt.show
+    plt.figure(figsize=(10, 4))
+    plt.plot(datos_participante["Tiempo"], datos_participante["Valor ECG"], color="blue", alpha=0.7)
+    plt.title(f"Gráfico de valores de ECG - Participante {datos_participante['ID participante'].iloc[0]}")
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Valor ECG")
+    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.show()
