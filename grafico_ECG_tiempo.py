d26f124f23762b8d5e488f9f5baf942fe78565f5
angiemarengo <marengoa@udesa.edu.ar>
2026-05-27 23:35:59 -0300

    Update grafico_ECG_tiempo

--- grafico_ECG_tiempo.py
+++ grafico_ECG_tiempo.py
@@ -7,6 +7,19 @@ Created on Wed May 27 21:57:43 2026
 
 import matplotlib.pyplot as plt
 def grafico_ECG_tiempo(datos_participante):
+    """
+    
+
+    Parameters
+    ----------
+    datos_participante : DataFrame
+       Es el DataFrame que contiene los datos del participante cuyo id es el id buscado .
+
+    Returns
+    -------
+    None.
+
+    """
     plt.figure()
     plt.plot(datos_participante["Tiempo"], datos_participante["Valor ECG"])
     plt.title("Gráfico de valores de ECG en función del tiempo")
